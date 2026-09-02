cbce051919adb56f4a146ac2a564b8700b32a376
boralabrodi2-dot <boralabrodi2@gmail.com>
2026-09-02 10:02:42 -0300

    update

---
+++ 21_manut_escala/escala_data.js
@@ -0,0 +1,597 @@
+const INITIAL_ESCALA_DATA = {
+  "periodo": "04JAN26 a 10JAN26",
+  "mes": "JANEIRO",
+  "ano": 2026,
+  "semana": 1,
+  "dias_datas": {
+    "dom": "4",
+    "seg": "5",
+    "ter": "6",
+    "qua": "7",
+    "qui": "8",
+    "sex": "9",
+    "sab": "10"
+  },
+  "grid": [
+    {
+      "num": 1,
+      "grupo": "EXPEDIENTE ADMINISTRATIVO DAS 08h às 18h",
+      "secao": "MANUTENÇÃO DE FROTA",
+      "funcao": "AUX SERV RAP E ALMOX",
+      "grad": "2º Sgt PM",
+      "re": "148267-0",
+      "nome": "AUGUSTO",
+      "dias": {
+        "dom": "F",
+        "seg": "S",
+        "ter": "S",
+        "qua": "S",
+        "qui": "S",
+        "sex": "S",
+        "sab": "F"
+      }
+    },
+    {
+      "num": 2,
+      "grupo": "EXPEDIENTE ADMINISTRATIVO DAS 08h às 18h",
+      "secao": "MANUTENÇÃO DE FROTA",
+      "funcao": "AUX SERV RAP E ALMOX",
+      "grad": "Cb PM",
+      "re": "125780-3",
+      "nome": "CLEBER",
+      "dias": {
+        "dom": "F",
+        "seg": "S",
+        "ter": "S",
+        "qua": "S",
+        "qui": "S",
+        "sex": "S",
+        "sab": "F"
+      }
+    },
+    {
+      "num": 3,
+      "grupo": "EXPEDIENTE ADMINISTRATIVO DAS 08h às 18h",
+      "secao": "MANUTENÇÃO DE FROTA",
+      "funcao": "AUX TEC",
+      "grad": "Cb PM",
+      "re": "142960-4",
+      "nome": "TIBURCIO",
+      "dias": {
+        "dom": "F",
+        "seg": "S",
+        "ter": "S",
+        "qua": "S",
+        "qui": "S",
+        "sex": "S",
+        "sab": "F"
+      }
+    },
+    {
+      "num": 4,
+      "grupo": "EXPEDIENTE ADMINISTRATIVO DAS 08h às 18h",
+      "secao": "MANUTENÇÃO DE FROTA",
+      "funcao": "AUX TEC",
+      "grad": "Cb PM",
+      "re": "122372-A",
+      "nome": "SOARES",
+      "dias": {
+        "dom": "F",
+        "seg": "S",
+        "ter": "S",
+        "qua": "S",
+        "qui": "S",
+        "sex": "S",
+        "sab": "F"
+      }
+    },
+    {
+      "num": 5,
+      "grupo": "EXPEDIENTE ADM. HORÁRIO 12 X 36 - DAS 07h às 19h",
+      "secao": "MANUTENÇÃO DE FROTA Equipe \"A\"",
+      "funcao": "AUX SERV RAP E ALMOX",
+      "grad": "2º Sgt PM",
+      "re": "111402-6",
+      "nome": "REIS",
+      "dias": {
+        "dom": "F",
+        "seg": "S",
+        "ter": "S",
+        "qua": "S",
+        "qui": "S",
+        "sex": "S",
+        "sab": "F"
+      }
+    },
+    {
+      "num": 6,
+      "grupo": "EXPEDIENTE ADM. HORÁRIO 12 X 36 - DAS 07h às 19h",
+      "secao": "MANUTENÇÃO DE FROTA Equipe \"A\"",
+      "funcao": "AUX TEC",
+      "grad": "2º Sgt PM",
+      "re": "118860-7",
+      "nome": "BRITO",
+      "dias": {
+        "dom": "F",
+        "seg": "S",
+        "ter": "S",
+        "qua": "S",
+        "qui": "S",
+        "sex": "S",
+        "sab": "F"
+      }
+    },
+    {
+      "num": 7,
+      "grupo": "EXPEDIENTE ADM. HORÁRIO 12 X 36 - DAS 07h às 19h",
+      "secao": "MANUTENÇÃO DE FROTA Equipe \"A\"",
+      "funcao": "AUX TEC",
+      "grad": "Cb PM",
+      "re": "113934-7",
+      "nome": "BARONE",
+      "dias": {
+        "dom": "F",
+        "seg": "S",
+        "ter": "S",
+        "qua": "S",
+        "qui": "S",
+        "sex": "S",
+        "sab": "F"
+      }
+    },
+    {
+      "num": 8,
+      "grupo": "EXPEDIENTE ADM. HORÁRIO 12 X 36 - DAS 07h às 19h",
+      "secao": "MANUTENÇÃO DE FROTA Equipe \"A\"",
+      "funcao": "AUX SERV RAP E ALMOX",
+      "grad": "Cb PM",
+      "re": "982262-3",
+      "nome": "ANDREA LIMA",
+      "dias": {
+        "dom": "F",
+        "seg": "S",
+        "ter": "S",
+        "qua": "S",
+        "qui": "S",
+        "sex": "S",
+        "sab": "F"
+      }
+    },
+    {
+      "num": 9,
+      "grupo": "EXPEDIENTE ADM. HORÁRIO 12 X 36 - DAS 07h às 19h",
+      "secao": "MANUTENÇÃO DE FROTA Equipe \"A\"",
+      "funcao": "AUX TEC",
+      "grad": "Cb PM",
+      "re": "132970-7",
+      "nome": "FAUSTINO",
+      "dias": {
+        "dom": "F",
+        "seg": "S",
+        "ter": "S",
+        "qua": "S",
+        "qui": "S",
+        "sex": "S",
+        "sab": "F"
+      }
+    },
+    {
+      "num": 10,
+      "grupo": "EXPEDIENTE ADM. HORÁRIO 12 X 36 - DAS 07h às 19h",
+      "secao": "MANUTENÇÃO DE FROTA Equipe \"A\"",
+      "funcao": "AUX SERV RAP E ALMOX",
+      "grad": "Cb PM",
+      "re": "139278-6",
+      "nome": "GASPAR",
+      "dias": {
+        "dom": "F",
+        "seg": "S",
+        "ter": "S",
+        "qua": "S",
+        "qui": "S",
+        "sex": "S",
+        "sab": "F"
+      }
+    },
+    {
+      "num": 11,
+      "grupo": "EXPEDIENTE ADM. HORÁRIO 12 X 36 - DAS 07h às 19h",
+      "secao": "MANUTENÇÃO DE FROTA Equipe \"A\"",
+      "funcao": "AUX SERV RAP E ALMOX",
+      "grad": "Cb PM",
+      "re": "140459-8",
+      "nome": "DANTAS",
+      "dias": {
+        "dom": "F",
+        "seg": "S",
+        "ter": "S",
+        "qua": "S",
+        "qui": "S",
+        "sex": "S",
+        "sab": "F"
+      }
+    },
+    {
+      "num": 12,
+      "grupo": "EXPEDIENTE ADM. HORÁRIO 12 X 36 - DAS 07h às 19h",
+      "secao": "MANUTENÇÃO DE FROTA Equipe \"A\"",
+      "funcao": "AUX SERV RAP E ALMOX",
+      "grad": "Cb PM",
+      "re": "121674-A",
+      "nome": "AIRON",
+      "dias": {
+        "dom": "F",
+        "seg": "S",
+        "ter": "S",
+        "qua": "S",
+        "qui": "S",
+        "sex": "S",
+        "sab": "F"
+      }
+    },
+    {
+      "num": 13,
+      "grupo": "EXPEDIENTE ADM. HORÁRIO 12 X 36 - DAS 07h às 19h",
+      "secao": "MANUTENÇÃO DE FROTA Equipe \"A\"",
+      "funcao": "AUX SERV RAP E ALMOX ALMOXARIFADO",
+      "grad": "Cb PM",
+      "re": "980694-6",
+      "nome": "TORQUATO",
+      "dias": {
+        "dom": "F",
+        "seg": "S",
+        "ter": "S",
+        "qua": "S",
+        "qui": "S",
+        "sex": "S",
+        "sab": "F"
+      }
+    },
+    {
+      "num": 14,
+      "grupo": "EXPEDIENTE ADM. HORÁRIO 12 X 36 - DAS 07h às 19h",
+      "secao": "MANUTENÇÃO DE FROTA Equipe \"A\"",
+      "funcao": "AUX SERV RAP E ALMOX",
+      "grad": "Cb PM",
+      "re": "143092-A",
+      "nome": "BAZONI",
+      "dias": {
+        "dom": "F",
+        "seg": "S",
+        "ter": "S",
+        "qua": "S",
+        "qui": "S",
+        "sex": "S",
+        "sab": "F"
+      }
+    },
+    {
+      "num": 15,
+      "grupo": "EXPEDIENTE ADM. HORÁRIO 12 X 36 - DAS 07h às 19h",
+      "secao": "MANUTENÇÃO DE FROTA Equipe \"A\"",
+      "funcao": "AUX SERV RAP E ALMOX",
+      "grad": "Sd PM",
+      "re": "153301-A",
+      "nome": "WILLIAM",
+      "dias": {
+        "dom": "F",
+        "seg": "S",
+        "ter": "S",
+        "qua": "S",
+        "qui": "S",
+        "sex": "S",
+        "sab": "F"
+      }
+    },
+    {
+      "num": 16,
+      "grupo": "EXPEDIENTE ADM. HORÁRIO 12 X 36 - DAS 07h às 19h",
+      "secao": "MANUTENÇÃO DE FROTA Equipe \"B\"",
+      "funcao": "AUX SERV RAP E ALMOX",
+      "grad": "2º Sgt PM",
+      "re": "107774-A",
+      "nome": "LUCIANO",
+      "dias": {
+        "dom": "F",
+        "seg": "S",
+        "ter": "S",
+        "qua": "S",
+        "qui": "S",
+        "sex": "S",
+        "sab": "F"
+      }
+    },
+    {
+      "num": 17,
+      "grupo": "EXPEDIENTE ADM. HORÁRIO 12 X 36 - DAS 07h às 19h",
+      "secao": "MANUTENÇÃO DE FROTA Equipe \"B\"",
+      "funcao": "AUX SERV RAP E ALMOX",
+      "grad": "Cb PM",
+      "re": "144643-6",
+      "nome": "SANTOS",
+      "dias": {
+        "dom": "F",
+        "seg": "S",
+        "ter": "S",
+        "qua": "S",
+        "qui": "S",
+        "sex": "S",
+        "sab": "F"
+      }
+    },
+    {
+      "num": 18,
+      "grupo": "EXPEDIENTE ADM. HORÁRIO 12 X 36 - DAS 07h às 19h",
+      "secao": "MANUTENÇÃO DE FROTA Equipe \"B\"",
+      "funcao": "AUX SERV RAP E ALMOX",
+      "grad": "Cb PM",
+      "re": "112223-1",
+      "nome": "MOURA",
+      "dias": {
+        "dom": "F",
+        "seg": "S",
+        "ter": "S",
+        "qua": "S",
+        "qui": "S",
+        "sex": "S",
+        "sab": "F"
+      }
+    },
+    {
+      "num": 19,
+      "grupo": "EXPEDIENTE ADM. HORÁRIO 12 X 36 - DAS 07h às 19h",
+      "secao": "MANUTENÇÃO DE FROTA Equipe \"B\"",
+      "funcao": "AUX TEC",
+      "grad": "Cb PM",
+      "re": "960331-0",
+      "nome": "NETO",
+      "dias": {
+        "dom": "F",
+        "seg": "S",
+        "ter": "S",
+        "qua": "S",
+        "qui": "S",
+        "sex": "S",
+        "sab": "F"
+      }
+    },
+    {
+      "num": 20,
+      "grupo": "EXPEDIENTE ADM. HORÁRIO 12 X 36 - DAS 07h às 19h",
+      "secao": "MANUTENÇÃO DE FROTA Equipe \"B\"",
+      "funcao": "AUX SERV RAP E ALMOX",
+      "grad": "Cb PM",
+      "re": "981737-9",
+      "nome": "ZULEICA",
+      "dias": {
+        "dom": "F",
+        "seg": "S",
+        "ter": "S",
+        "qua": "S",
+        "qui": "S",
+        "sex": "S",
+        "sab": "F"
+      }
+    },
+    {
+      "num": 21,
+      "grupo": "EXPEDIENTE ADM. HORÁRIO 12 X 36 - DAS 07h às 19h",
+      "secao": "MANUTENÇÃO DE FROTA Equipe \"B\"",
+      "funcao": "AUX SERV RAP E ALMOX",
+      "grad": "Cb PM",
+      "re": "982379-4",
+      "nome": "JULIANA",
+      "dias": {
+        "dom": "F",
+        "seg": "S",
+        "ter": "S",
+        "qua": "S",
+        "qui": "S",
+        "sex": "S",
+        "sab": "F"
+      }
+    },
+    {
+      "num": 22,
+      "grupo": "EXPEDIENTE ADM. HORÁRIO 12 X 36 - DAS 07h às 19h",
+      "secao": "MANUTENÇÃO DE FROTA Equipe \"B\"",
+      "funcao": "AUX SERV RAP E ALMOX",
+      "grad": "Cb PM",
+      "re": "111264-3",
+      "nome": "GALVÃO",
+      "dias": {
+        "dom": "F",
+        "seg": "S",
+        "ter": "S",
+        "qua": "S",
+        "qui": "S",
+        "sex": "S",
+        "sab": "F"
+      }
+    },
+    {
+      "num": 23,
+      "grupo": "EXPEDIENTE ADM. HORÁRIO 12 X 36 - DAS 07h às 19h",
+      "secao": "MANUTENÇÃO DE FROTA Equipe \"B\"",
+      "funcao": "AUX SERV RAP E ALMOX",
+      "grad": "Cb PM",
+      "re": "121441-1",
+      "nome": "THIAGO",
+      "dias": {
+        "dom": "F",
+        "seg": "S",
+        "ter": "S",
+        "qua": "S",
+        "qui": "S",
+        "sex": "S",
+        "sab": "F"
+      }
+    },
+    {
+      "num": 24,
+      "grupo": "EXPEDIENTE ADM. HORÁRIO 12 X 36 - DAS 07h às 19h",
+      "secao": "MANUTENÇÃO DE FROTA Equipe \"B\"",
+      "funcao": "AUX TEC",
+      "grad": "Cb PM",
+      "re": "151168-8",
+      "nome": "SANTIAGO",
+      "dias": {
+        "dom": "F",
+        "seg": "S",
+        "ter": "S",
+        "qua": "S",
+        "qui": "S",
+        "sex": "S",
+        "sab": "F"
+      }
+    },
+    {
+      "num": 25,
+      "grupo": "EXPEDIENTE ADM. HORÁRIO 12 X 36 - DAS 07h às 19h",
+      "secao": "MANUTENÇÃO DE FROTA Equipe \"B\"",
+      "funcao": "AUX SERV RAP E ALMOX",
+      "grad": "Cb PM",
+      "re": "130420-8",
+      "nome": "EDUARDO",
+      "dias": {
+        "dom": "F",
+        "seg": "S",
+        "ter": "S",
+        "qua": "S",
+        "qui": "S",
+        "sex": "S",
+        "sab": "F"
+      }
+    },
+    {
+      "num": 26,
+      "grupo": "EXPEDIENTE ADM. HORÁRIO 12 X 36 - DAS 07h às 19h",
+      "secao": "MANUTENÇÃO DE FROTA Equipe \"B\"",
+      "funcao": "AUX SERV RAP E ALMOX",
+      "grad": "Sd PM",
+      "re": "157019-6",
+      "nome": "RONALDO",
+      "dias": {
+        "dom": "F",
+        "seg": "S",
+        "ter": "S",
+        "qua": "S",
+        "qui": "S",
+        "sex": "S",
+        "sab": "F"
+      }
+    },
+    {
+      "num": 27,
+      "grupo": "HORÁRIO 12 X 36 - DAS 06h às 18h",
+      "secao": "MANUTENÇÃO DE VEÍCULO PESADO",
+      "funcao": "MOTORISTA VTR",
+      "grad": "Cb PM",
+      "re": "106571-8",
+      "nome": "CARLOS",
+      "dias": {
+        "dom": "F",
+        "seg": "S",
+        "ter": "S",
+        "qua": "S",
+        "qui": "S",
+        "sex": "S",
+        "sab": "F"
+      }
+    },
+    {
+      "num": 28,
+      "grupo": "HORÁRIO 12 X 36 - DAS 06h às 18h",
+      "secao": "MANUTENÇÃO DE VEÍCULO PESADO",
+      "funcao": "MOTORISTA VTR PESADA",
+      "grad": "Cb PM",
+      "re": "109836-5",
+      "nome": "GARCIA",
+      "dias": {
+        "dom": "F",
+        "seg": "S",
+        "ter": "S",
+        "qua": "S",
+        "qui": "S",
+        "sex": "S",
+        "sab": "F"
+      }
+    },
+    {
+      "num": 29,
+      "grupo": "HORÁRIO 12 X 36 - DAS 06h às 18h",
+      "secao": "MANUTENÇÃO DE VEÍCULO PESADO",
+      "funcao": "MOTORISTA VTR PESADA",
+      "grad": "Cb PM",
+      "re": "121728-3",
+      "nome": "ALEXANDRE N.",
+      "dias": {
+        "dom": "F",
+        "seg": "S",
+        "ter": "S",
+        "qua": "S",
+        "qui": "S",
+        "sex": "S",
+        "sab": "F"
+      }
+    },
+    {
+      "num": 30,
+      "grupo": "HORÁRIO 12 X 36 - DAS 06h às 18h",
+      "secao": "MANUTENÇÃO DE VEÍCULO PESADO",
+      "funcao": "MOTORISTA VTR PESADA",
+      "grad": "Cb PM",
+      "re": "143641-4",
+      "nome": "MACIEL",
+      "dias": {
+        "dom": "F",
+        "seg": "S",
+        "ter": "S",
+        "qua": "S",
+        "qui": "S",
+        "sex": "S",
+        "sab": "F"
+      }
+    }
+  ],
+  "afastamentos": [],
+  "observacoes": [],
+  "restricoes": [],
+  "qpo": {
+    "subten_sgt": {
+      "fixado": 8,
+      "existente": 4
+    },
+    "cb_sd": {
+      "fixado": 21,
+      "existente": 26
+    },
+    "total": {
+      "fixado": 29,
+      "existente": 30
+    }
+  },
+  "aux_oficial": {
+    "titular": "3º Sgt PM 191299-2 Nathalia",
+    "substituto": "3º Sgt PM 105043-5 Serapião"
+  },
+  "assinaturas": {
+    "elaborador": {
+      "nome": "LUCIANO PEREIRA DE SOUZA",
+      "cargo": "2º SGT PM AUX SEÇ MANUT FROTA"
+    },
+    "conferente": {
+      "nome": "JULIANA MARINO",
+      "cargo": "CB PM ESCAL SEÇ MANUT FROTA"
+    },
+    "chefe_secao": {
+      "nome": "REBECA COSTA ANDRADE MELO",
+      "cargo": "1º TEN PM CH SEÇ MANUT FROTA"
+    },
+    "chefe_divisao": {
+      "nome": "SAMUEL DE ANDRADE",
+      "cargo": "MAJ PM CH DIV MANUT TRANSP"
+    }
+  },
+  "alteracoes": []
+};
